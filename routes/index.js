7330644fb231ca360d234ab41bbbd0d06e4d338f
Marcos Teixeira <marcos.teixeira13@fatec.sp.gov.br>
2019-11-13 11:50:38 -0300

    Changed the link structure

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,27 @@ const router = express.Router()
 // Require controller modules.
 const pacienteController = require('../controllers/pacienteController.js')
 
-/* GET home page. */
-router.get('/', pacienteController.index)
+// GET request for list of all Pacientes.
+router.get('/', pacienteController.paciente_list)
 
-router.get('/paciente/create', pacienteController.paciente_create_get)
+router.get('/create', pacienteController.paciente_create_get)
 
 // POST request for creating Paciente.
-router.post('/paciente/create', pacienteController.paciente_create_post)
+router.post('/create', pacienteController.paciente_create_post)
 
 // GET request to delete Paciente.
-router.get('/paciente/:id/delete', pacienteController.paciente_delete_get)
+router.get('/:id/delete', pacienteController.paciente_delete_get)
 
 // POST request to delete Paciente.
-router.post('/paciente/:id/delete', pacienteController.paciente_delete_post)
+router.post('/:id/delete', pacienteController.paciente_delete_post)
 
 // GET request to update Paciente.
-router.get('/paciente/:id/update', pacienteController.paciente_update_get)
+router.get('/:id/update', pacienteController.paciente_update_get)
 
 // POST request to update Paciente.
-router.post('/paciente/:id/update', pacienteController.paciente_update_post)
+router.post('/:id/update', pacienteController.paciente_update_post)
 
 // GET request for one Paciente.
-router.get('/paciente/:id', pacienteController.paciente_detail)
-
-// GET request for list of all Pacientes.
-router.get('/pacientes', pacienteController.paciente_list)
+router.get('/:id', pacienteController.paciente_detail)
 
 module.exports = router
